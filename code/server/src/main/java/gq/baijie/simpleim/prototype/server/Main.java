2daa3faa5ebd7c414830c5c5cd5f0623a10df7b5
Bai Jie <baijie1991@gmail.com>
2016-06-29 15:51:04 +0800

    add 'server' subproject

---
+++ code/server/src/main/java/gq/baijie/simpleim/prototype/server/Main.java
@@ -0,0 +1,11 @@
+package gq.baijie.simpleim.prototype.server;
+
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+  public static void main(String[] args) {
+    LoggerFactory.getLogger(Main.class).info("Hello World!");
+  }
+
+}
