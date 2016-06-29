9673b307246ea4a8b897fdc98a0d1ef165ad112d
Bai Jie <baijie1991@gmail.com>
2016-06-29 15:51:04 +0800

    update message structure

--- code/server/src/main/java/gq/baijie/simpleim/prototype/server/Main.java
+++ code/server/src/main/java/gq/baijie/simpleim/prototype/server/Main.java
@@ -1,14 +1,64 @@
 package gq.baijie.simpleim.prototype.server;
 
-import gq.baijie.simpleim.prototype.server.proto.message.MessageFrameOuterClass;
+import com.google.protobuf.Any;
+
+import gq.baijie.simpleim.prototype.server.proto.message.Message;
 
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println(MessageFrameOuterClass.MessageFrame.newBuilder()
-                           .setSessionId(1)
-                           .setServiceId("test service")
-                           .build());
+    final Message.Request request = Message.Request.newBuilder()
+        .setId(1)
+        .setFunction("test function")
+        .build();
+    println(request);
+
+    Message.Frame frame = Message.Frame.newBuilder()
+        .setRequest(request)
+        .build();
+    println(frame);
+    println(frame.getMessageCase());
+    println(frame.getRequest().toByteString());
+
+    Message.Response response = Message.Response.newBuilder()
+        .setId(1)
+        .setSuccessMessage(Any.pack(request))
+        .build();
+    println(response);
+    println(response.getResultCase());
+    println(response.getSuccessMessage().getClass());
+    println(response.getSuccessMessage().toByteString());
+    println(response.getSuccessMessage());
+    println(response.getFailureCause().getClass());
+    println(response.getFailureCause().toByteString());
+    println(response.getFailureCause());
+
+    frame = Message.Frame.newBuilder()
+        .setResponse(response)
+        .build();
+    println(frame);
+    println(frame.getMessageCase());
+//    println(frame.getRequest().toByteString());
+
+    response = Message.Response.newBuilder()
+        .setId(1)
+        .setSuccessMessage(Any.pack(request))
+        .setFailureCause(Any.pack(request))
+        .build();
+    println(response);
+    println(response.getResultCase());
+    println(response.getSuccessMessage().getClass());
+    println(response.getSuccessMessage().toByteString());
+    println(response.getSuccessMessage());
+    println(response.getFailureCause().getClass());
+    println(response.getFailureCause().toByteString());
+    println(response.getFailureCause());
+
+
+  }
+
+  private static void println(Object o) {
+    System.out.println(o);
   }
 
 }
