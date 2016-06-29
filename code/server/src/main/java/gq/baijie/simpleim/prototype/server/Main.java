7544c7ed5f641dae8e7ce812c5f00d8e4d557e90
Bai Jie <baijie1991@gmail.com>
2016-06-29 15:51:04 +0800

    use Protocol Buffers

--- code/server/src/main/java/gq/baijie/simpleim/prototype/server/Main.java
+++ code/server/src/main/java/gq/baijie/simpleim/prototype/server/Main.java
@@ -1,11 +1,14 @@
 package gq.baijie.simpleim.prototype.server;
 
-import org.slf4j.LoggerFactory;
+import gq.baijie.simpleim.prototype.server.proto.message.MessageFrameOuterClass;
 
 public class Main {
 
   public static void main(String[] args) {
-    LoggerFactory.getLogger(Main.class).info("Hello World!");
+    System.out.println(MessageFrameOuterClass.MessageFrame.newBuilder()
+                           .setSessionId(1)
+                           .setServiceId("test service")
+                           .build());
   }
 
 }
